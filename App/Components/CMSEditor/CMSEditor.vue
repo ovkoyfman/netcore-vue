00216a6acbf62e3025fbbb63563eb7ea0e05f876
Oksana Koyfman <okildavdan@comcast.net>
2018-10-23 08:48:22 -0400

    Fixed sidebar

--- App/Components/CMSEditor/CMSEditor.vue
+++ App/Components/CMSEditor/CMSEditor.vue
@@ -13,6 +13,32 @@
       <router-view class="cmseditor"></router-view>
     </div>
   </div>
+  <b-modal id="modal1" title="Manage Pages">
+    <!-- <b-form @submit.prevent>
+      <ul>
+      <li v-for="(item, index) in navData">
+      
+      <b-form-checkbox :value="item.title" value="item.checked"/><label>{{item.title}}</label>
+      <ul>
+      <li v-for="(item, index) in item.subpages">
+      <b-form-checkbox :value="item.title" :checked="item.checked"/><label>{{item.title}}</label></li>
+      </ul>
+      </li>
+      </ul>
+    </b-form> -->
+    <b-form @submit.prevent>
+      <ul>
+      <li v-for="(item, index) in navData">
+      
+      <input type="checkbox" :value="item.title" :checked="item.checked" @click="parentClick(item)" :disabled="item.disabled"/><label>{{item.title}}</label>
+      <ul>
+      <li v-for="(subitem, index) in item.subpages">
+      <input type="checkbox" :value="subitem.title" :checked="subitem.checked" @click.prevent="childClick(item,subitem)" :disabled="subitem.disabled"/><label>{{subitem.title}}</label></li>
+      </ul>
+      </li>
+      </ul>
+    </b-form>
+  </b-modal>
 </div>
 </template>
 <script type="text/javascript">
@@ -49,6 +75,27 @@ export default {
    computed: {
     linkHref: function() {
       return this.$store.state.styleLink;
+    },
+    navData: function() {
+      return this.$store.state.menuData;
+    }
+  },
+   methods: {
+    parentClick: function(item) {
+      item.checked = !item.checked;
+      item.subpages.forEach(element => {
+        element.checked = item.checked;
+        console.log(item.checked);
+      });
+    },
+    childClick: function(parentItem, childItem) {
+      var flag = 0;
+      childItem.checked = !childItem.checked;
+      parentItem.subpages.forEach(element => {
+        if (element.checked) flag = 1;
+        console.log("flag", flag);
+      });
+      parentItem.checked = flag ? true : false;
     }
   }
   // created(){
--- App/Components/CMSEditor/Welcome.vue
+++ App/Components/CMSEditor/Welcome.vue
@@ -3,32 +3,6 @@
     <div>
 
   <!-- Modal Component -->
-  <b-modal id="modal1" title="Manage Pages">
-    <!-- <b-form @submit.prevent>
-      <ul>
-      <li v-for="(item, index) in navData">
-      
-      <b-form-checkbox :value="item.title" value="item.checked"/><label>{{item.title}}</label>
-      <ul>
-      <li v-for="(item, index) in item.subpages">
-      <b-form-checkbox :value="item.title" :checked="item.checked"/><label>{{item.title}}</label></li>
-      </ul>
-      </li>
-      </ul>
-    </b-form> -->
-    <b-form @submit.prevent>
-      <ul>
-      <li v-for="(item, index) in navData">
-      
-      <input type="checkbox" :value="item.title" :checked="item.checked" @click="parentClick(item)" :disabled="item.disabled"/><label>{{item.title}}</label>
-      <ul>
-      <li v-for="(subitem, index) in item.subpages">
-      <input type="checkbox" :value="subitem.title" :checked="subitem.checked" @click.prevent="childClick(item,subitem)" :disabled="subitem.disabled"/><label>{{subitem.title}}</label></li>
-      </ul>
-      </li>
-      </ul>
-    </b-form>
-  </b-modal>
 </div>
     <div class="header-image">
       <h2>Hotel Origami Proposal</h2>
@@ -54,9 +28,6 @@ export default {
   computed: {
     formData: function() {
       return this.$store.state.dataForTheForm;
-    },
-    navData: function() {
-      return this.$store.state.menuData;
     }
     // edit: function() {
     //   return this.$store.state.editableStateForThePage;
@@ -65,22 +36,6 @@ export default {
   methods: {
     transformToNotEditable: function() {
       this.$store.commit("transformToNotEditable", this.formData.components);
-    },
-    parentClick: function(item) {
-      item.checked = !item.checked;
-      item.subpages.forEach(element => {
-        element.checked = item.checked;
-        console.log(item.checked);
-      });
-    },
-    childClick: function(parentItem, childItem) {
-      var flag = 0;
-      childItem.checked = !childItem.checked;
-      parentItem.subpages.forEach(element => {
-        if (element.checked) flag = 1;
-        console.log("flag", flag);
-      });
-      parentItem.checked = flag ? true : false;
     }
   }
 };
